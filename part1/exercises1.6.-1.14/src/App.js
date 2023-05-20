3da301526d3a5ebc386317557bc86e44f3268ede
Iury <iurymacali0101@hotmail.com>
2023-05-20 17:15:01 -0300

    exercise 1.9

--- part1/exercises1.6.-1.14/src/App.js
+++ part1/exercises1.6.-1.14/src/App.js
@@ -57,18 +57,53 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [all, setAll] = useState(0);
+  const [displayOn, setDisplayOn] = useState(false);
 
   return (
-    <Statistics
-      good={good}
-      setGood={setGood}
-      neutral={neutral}
-      setNeutral={setNeutral}
-      bad={bad}
-      setBad={setBad}
-      all={all}
-      setAll={setAll}
-    />
+    <>
+      {displayOn ? (
+        <Statistics
+          good={good}
+          setGood={setGood}
+          neutral={neutral}
+          setNeutral={setNeutral}
+          bad={bad}
+          setBad={setBad}
+          all={all}
+          setAll={setAll}
+        />
+      ) : (
+        <div>
+          <h2>Give feedback</h2>
+          <Button
+            text="good"
+            onClick={() => {
+              setGood((prevState) => prevState + 1);
+              setAll((prevState) => prevState + 1);
+              setDisplayOn(true);
+            }}
+          />
+          <Button
+            text="neutral"
+            onClick={() => {
+              setNeutral((prevState) => prevState + 1);
+              setAll((prevState) => prevState + 1);
+              setDisplayOn(true);
+            }}
+          />
+          <Button
+            text="bad"
+            onClick={() => {
+              setBad((prevState) => prevState + 1);
+              setAll((prevState) => prevState + 1);
+              setDisplayOn(true);
+            }}
+          />
+          <h2>Statistics</h2>
+          <p>No feedback given</p>
+        </div>
+      )}
+    </>
   );
 };
 
