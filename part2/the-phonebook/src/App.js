57a6e2c9a7968e11611de35b337114cdc060b43f
Iury <iurymacali0101@hotmail.com>
2023-06-01 18:09:16 -0300

    the Phonebook Step3

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -3,18 +3,23 @@ import { useState } from 'react';
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
   };
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
 
   const addPerson = (e) => {
     e.preventDefault();
-    if (!newName) {
+    if (!newName || !newNumber) {
       return alert('empty input');
     }
     const personObj = {
       name: newName,
+      number: newNumber,
     };
 
     if (persons.find((p) => p.name === newName)) {
@@ -22,6 +27,7 @@ const App = () => {
       return;
     }
     setPersons(persons.concat(personObj));
+    setNewNumber('');
     setNewName('');
   };
 
@@ -33,12 +39,17 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((p) => (
-        <div key={p.name}>{p.name}</div>
+        <div key={p.name}>
+          {p.name} {p.number}
+        </div>
       ))}
     </div>
   );
